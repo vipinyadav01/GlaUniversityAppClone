5104075f0ea3b8ef2286fcaea1d4e044475c2a2b
vipinyadav01 <vipinyadav9m@gmail.com>
2025-02-26 23:56:48 +0530

    Add BottomNavigation component to App and integrate it into routing

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Message from "./components/Message";
 import Profile from "./components/Profile";
 import Notification from "./components/Notification";
 import TimeTable from "./pages/Timetable";
+import BottomNavigation from "./components/BottomNavigation";
 import SubjectiveQuestions from "./components/SubjectiveQuestions";
 import QuickActionPanel from "./components/QuickActionPanel";
 import TodaysLectures from "./components/TodaysLectures";
@@ -20,30 +21,31 @@ import RegisterPage from "./pages/RegisterPage";
 import NotFound from "./pages/NotFound";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/attendance" element={<AttendancePage />} />
-        <Route path="/Fee-Details" element={<FeeDetail />} />
-        <Route path="/hierarchy" element={<HierarchyPage />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/library" element={<Library />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/placement" element={<Placement />} />
-        <Route path="/messages" element={<Message />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/notifications" element={<Notification />} />
-        <Route path="/timetable" element={<TimeTable />} />
-        <Route path="/subjective-quiz" element={<SubjectiveQuestions />} />
-        <Route path="/quick-action-panel" element={<QuickActionPanel />} />
-        <Route path="/todays-lectures" element={<TodaysLectures />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/attendance" element={<AttendancePage />} />
+                <Route path="/Fee-Details" element={<FeeDetail />} />
+                <Route path="/hierarchy" element={<HierarchyPage />} />
+                <Route path="/results" element={<ResultsPage />} />
+                <Route path="/library" element={<Library />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/placement" element={<Placement />} />
+                <Route path="/messages" element={<Message />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/notifications" element={<Notification />} />
+                <Route path="/timetable" element={<TimeTable />} />
+                <Route path="/subjective-quiz" element={<SubjectiveQuestions />} />
+                <Route path="/quick-action-panel" element={<QuickActionPanel />} />
+                <Route path="/todays-lectures" element={<TodaysLectures />} />
+                <Route path="/login" element={<LoginPage />} />
+                <BottomNavigation activeTab={activeTab} setActiveTab={setActiveTab} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
